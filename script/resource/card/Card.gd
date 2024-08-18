a6631c491c68c197f3b9cde91da3a51afe074569
booleto <exaccxd@gmail.com>
2024-08-19 00:00:02 +0700

    basic UI omg i suck ass at UI

--- script/resource/card/Card.gd
+++ script/resource/card/Card.gd
@@ -2,9 +2,13 @@ extends Resource
 class_name Card
 
 enum CardType {BUILD, EFFECT}
+enum CardFrame {HOUSE, HOSPITAL, MARKET, SUPERMARKET, SCHOOL, COMPANY}
 
 @export var card_name : String
 @export var energy_cost : int
 @export var ui_scene : PackedScene
 @export var type : CardType
 @export var effect : CardEffect
+
+@export var frame : CardFrame
+@export_multiline var tooltip : String
