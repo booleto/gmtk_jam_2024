59f6be8d6524c50ce640d0aef4d42b30e269cbac
nthung-2k5 <hungthitkhia@gmail.com>
2024-08-18 22:16:18 +0700

    modular go brr

--- script/entity/cardmanager/CardManager.gd
+++ script/entity/cardmanager/CardManager.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node
 class_name CardManager
 
@@ -5,7 +6,7 @@ signal deck_shuffled
 signal card_played(card: Card)
 signal card_drawn(card: Card)
 
-@export var deck_starters : PackedInt32Array
+#@export var deck_starters : PackedInt32Array
 @export var hand_limit : int
 @export var deck_size : int
 
@@ -15,23 +16,63 @@ var hand : Array[Card] = []
 var discard_pile : Array[Card] = []
 var deck : Array[Card] = []
 
+var cards: Dictionary = {}
+var card_starters: Dictionary = {}
+
+func _init() -> void:
+	for data in DirAccess.get_files_at("res://script/resource/card"):
+		if data.ends_with("Card.tres"):
+			var card: Card = load("res://script/resource/card/" + data)
+			cards[card.card_name] = card
+
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		notify_property_list_changed()
+		return
+	#_get_property_list()
 	await get_parent().ready
 	entity_manager = get_parent()
 	initialize_deck()
-	
+
+func _get(property: StringName) -> Variant:
+	if property.ends_with("Starters"):
+		var name = property.trim_suffix(" Starters")
+		return card_starters[name]
+	return null
+
+func _set(property: StringName, value: Variant) -> bool:
+	if property.ends_with("Starters"):
+		var name = property.trim_suffix(" Starters")
+		card_starters[name] = value
+		return true
+	return false
+
+func _get_property_list():
+	return cards.keys().map(func (name: String) -> Dictionary:
+		return {
+			"name": "%s Starters" % name,
+			"type": TYPE_INT,
+			"usage": PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE
+		}
+	)
 
 func initialize_deck(): # TODO: remove placeholder
-	for i in range(deck_starters[0]):
-		deck.append(DataIndex.CARD_COMPANY)
-	for i in range(deck_starters[1]):
-		deck.append(DataIndex.CARD_HOSPITAL)
-	for i in range(deck_starters[2]):
-		deck.append(DataIndex.CARD_HOUSE)
-	for i in range(deck_starters[3]):
-		deck.append(DataIndex.CARD_MARKET)
-	for i in range(deck_starters[4]):
-		deck.append(DataIndex.CARD_SCHOOL)
+	#for i in range(deck_starters[0]):
+		#deck.append(DataIndex.CARD_COMPANY)
+	#for i in range(deck_starters[1]):
+		#deck.append(DataIndex.CARD_HOSPITAL)
+	#for i in range(deck_starters[2]):
+		#deck.append(DataIndex.CARD_HOUSE)
+	#for i in range(deck_starters[3]):
+		#deck.append(DataIndex.CARD_MARKET)
+	#for i in range(deck_starters[4]):
+		#deck.append(DataIndex.CARD_SCHOOL)
+	for name in card_starters:
+		var card = cards[name]
+		var i = card_starters[name]
+		for j in range(i):
+			deck.append(card)
+	
 	deck.shuffle()
 	fill_hand()
 	print_hand()
