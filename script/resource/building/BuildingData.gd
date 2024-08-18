4845813f92ba08553098a0fe878c54b6604c7ab3
nthung-2k5 <hungthitkhia@gmail.com>
2024-08-18 17:25:22 +0700

    absolute modular

--- script/resource/building/BuildingData.gd
+++ script/resource/building/BuildingData.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name BuildingData
 
+@export var input_action: String
 @export var building_name : String
 @export var sprite : Texture2D
 @export var card_sprite : Texture2D
