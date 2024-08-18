efcbe0c69a0891b1c72ada25afb8174177b5e547
nthung-2k5 <hungthitkhia@gmail.com>
2024-08-18 16:34:59 +0700

    fakk gdscript

--- script/entity/building/effect/HouseEffect.gd
+++ script/entity/building/effect/HouseEffect.gd
@@ -4,5 +4,5 @@ class_name HouseEffect
 func calc_bonus(adjacent_builds : Array[Building], args : Dictionary) -> CityResource:
 	var turn : int = args["turn"]
 	
-	var resource = CityResource.new(0, -2, 0, 0, 0) if turn % 2 == 1 else CityResource.new(0, 0, 0, 0, 0)
+	var resource = CityResource.new(-2, 0, 0, 0) if turn % 2 == 1 else CityResource.new(0, 0, 0, 0)
 	return resource
