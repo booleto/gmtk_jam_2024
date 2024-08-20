81f80fc27f5d074a7d9dc81589c5812af5699ba8
nthung-2k5 <hungthitkhia@gmail.com>
2024-08-20 22:43:08 +0700

    friendship ended with AnimationPlayer, now Tween is my best friend

--- scene/UI/money_counter.gd
+++ scene/UI/money_counter.gd
@@ -1,9 +1,16 @@
 extends NinePatchRect
-
+var tween: Tween
 @onready var label: Label = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.money_changed.connect(on_population_update)
 
 func on_population_update(new_amount: int): 
+	var old_amount := int(label.text)
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_method(set_label_text, old_amount, new_amount, abs(new_amount - old_amount) / 30.0)
+
+func set_label_text(new_amount: int):
 	label.text = str(new_amount)
